85e8e4e8e6d6528c27192d8a49eca26542a6a54b
Valerie Craig <valine@gmail.com>
2016-06-20 21:24:51 -0700

    work on DOM

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
 // Store the results for each location in a separate array... perhaps as a property of the object representing that location
 // Display the values of each array as unordered lists in the browser
-// Calculating the sum of these hourly totals; your output for each location should look like this:
+
 var hours = ['6:00am', '7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm', '1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm','8:00pm'];
 // Object template for cookie stand.
 var shopAlki = {
@@ -28,14 +28,21 @@ shopAlki.custArray = function() {
   }
 };
 
-//var shopAlki.hourlyCook[0] =  shopAlki.hourlyCust[0] * shopAlki.cookAvg
-
+shopAlki.salesData = function() {
+  //Access the Dom element for data position
+  var dataList = document.getElementById('alkiData');
+  for (var i = 0; i < hours.length; i++) {
+    //create the html element
+    var listItem = document.createElement('li');
+    //distribute content to the element
+    listItem.textContent = hours[i] + shopAlki.hourlyArray.push() + ' cookies';
+    console.log(listItem.textContent);
+    //append the element to the Dom
+    alkiData.appendChild(listItem);
+  }
+};
+shopAlki.salesData();
 
 shopAlki.custArray();
-console.log(shopAlki.hourlyCust + ' shopAlki.hourlyCust');
+// console.log(shopAlki.hourlyCust + ' shopAlki.hourlyCust');
 console.log(shopAlki.hourlyArray + ' shopAlki.hourlyArray');
-
-
-
-
-  // prop for hourly sales [] <-- method to calc this array
