e9f91094fb36514ad77108a89998699283c6611a
Derek Hewitt <derekmhewitt@gmail.com>
2016-06-22 16:31:41 -0700

    mvp achieved, still needs some cleanup

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // cookie-stand
 
 var hours = ['6:00am', '7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm', '1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm','8:00pm'];
+var shops = [];
+var addStoreForm = document.getElementById('add_store_form');
+
 // construct object for cookie stand.
 function Shop(shopLoc, custPerHrMin, custPerHrMax, cookPerCustAvg) {
   this.shopLoc = shopLoc;
@@ -11,8 +14,23 @@ function Shop(shopLoc, custPerHrMin, custPerHrMax, cookPerCustAvg) {
   this.custPerHrArray = []; // array containing a random number of customers per hour.
   this.cooksPerHrArray = []; // array containing number of cookies sold in an hour period.
   this.totalDailyCookies = 0;
+  shops.push(this);
   // this.randNum = 0;
 }
+
+addStoreForm.addEventListener('submit', function(event){
+  event.preventDefault();
+  var minCustPerHour = parseInt(document.getElementById('min_cust_per_hour').value),
+    maxCustPerHour = parseInt(document.getElementById('max_cust_per_hour').value),
+    avgCookiesPerCust = parseFloat(document.getElementById('avg_cookies_per_hour').value),
+    storeName = document.getElementById('shop_location').value;
+    //here is where data validation will go if we have time
+  var someThine = new Shop(storeName, minCustPerHour, maxCustPerHour, avgCookiesPerCust);
+  var dataTable = document.getElementById('salesData');
+  dataTable.textContent = '';
+  headerRow();
+  renderShopsArray();
+});
 
 var firstAndPike = new Shop('1st and Pike', 23, 65, 6.3);
 var seatacAirport = new Shop('SeaTac Airport', 3, 24, 1.2);
@@ -38,55 +56,30 @@ Shop.prototype.cooksPerHr = function() {
   // console.log(this.cooksPerHrArray);
 };
 
-firstAndPike.cooksPerHr();
-seatacAirport.cooksPerHr();
-seattleCenter.cooksPerHr();
-capitolHill.cooksPerHr();
-alki.cooksPerHr();
+// firstAndPike.cooksPerHr();
+// seatacAirport.cooksPerHr();
+// seattleCenter.cooksPerHr();
+// capitolHill.cooksPerHr();
+// alki.cooksPerHr();
 
-// firstAndPike.cooksPerHr(); //calling the instance
-//console.log(firstAndPike.cooksPerHrArray);
-
-// render the table heading row
-Shop.prototype.headerRow = function() {
-  //Access the Dom element for data position
-  var dataTable = document.getElementById('salesData');
-
-  var trEl = document.createElement('tr');  //create the html element
-
-  var thEl = document.createElement('th'); //create the html element
-  thEl.textContent = '';  // fill
-  dataTable.appendChild(thEl);  // append
-
-  var thEl = document.createElement('th');
-  thEl.textContent = 'Daily Location Total';  // fill
-  dataTable.appendChild(thEl);  // append
-
-  for (var i = 0; i < hours.length; i++) {
-    var thEl = document.createElement('th');
-    thEl.textContent = hours[i];  // fill
-    dataTable.appendChild(thEl);  // append
-  }
-  dataTable.appendChild(trEl);  // append
-};
-Shop.prototype.headerRow();
+//sams example of clearing comments, we will use to clear table
+// clearChatList.addEventListener('click', function() {
+//   chatList.innerHTML = '';
+//   console.log('You just cleared the chat list!');
+//   allComments = [];
+// });
 
 // render the table
 Shop.prototype.renderData = function() {
-  //Access the Dom element for data position
-
+  this.cooksPerHr();
   var dataTable = document.getElementById('salesData');
-
   var trEl = document.createElement('tr');  //create the html element
-
   var tdEl = document.createElement('td'); //create the html element
   tdEl.textContent = this.shopLoc;  // fill
   dataTable.appendChild(tdEl);  // append
-
   var tdEl = document.createElement('td');
   tdEl.textContent = this.totalDailyCookies;  // fill
   dataTable.appendChild(tdEl);  // append
-
   for (var i = 0; i < hours.length; i++) {
     var tdEl = document.createElement('td');
     tdEl.textContent = this.cooksPerHrArray[i];  // fill
@@ -94,9 +87,30 @@ Shop.prototype.renderData = function() {
   }
   dataTable.appendChild(trEl);  // append
 };
+// render the table heading row
+var headerRow = function() {
+  //Access the Dom element for data position
+  var dataTable = document.getElementById('salesData');
+  var trEl = document.createElement('tr');  //create the html element
+  var thEl = document.createElement('th'); //create the html element
+  thEl.textContent = '';  // fill
+  dataTable.appendChild(thEl);  // append
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Daily Location Total';  // fill
+  dataTable.appendChild(thEl);  // append
+  for (var i = 0; i < hours.length; i++) {
+    var thEl = document.createElement('th');
+    thEl.textContent = hours[i];  // fill
+    dataTable.appendChild(thEl);  // append
+  }
+  dataTable.appendChild(trEl);  // append
+};
 
-firstAndPike.renderData();
-seatacAirport.renderData();
-seattleCenter.renderData();
-capitolHill.renderData();
-alki.renderData();
+var renderShopsArray = function() {
+  for(var i = 0; i < shops.length; i++) {
+    shops[i].renderData(); //you can add i later to shade even rows
+  }
+};
+//call functions here
+headerRow();
+renderShopsArray();
